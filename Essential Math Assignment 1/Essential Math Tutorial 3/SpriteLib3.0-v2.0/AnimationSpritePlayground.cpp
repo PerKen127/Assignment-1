f2a0aad5f52d0ad326592695dcd7c2b2b73ec36d
Josh <jkeny127@gmai.com>
2020-10-14 20:00:38 -0400

    Project Start

---
+++ Essential Math Assignment 1/Essential Math Tutorial 3/SpriteLib3.0-v2.0/AnimationSpritePlayground.cpp
@@ -0,0 +1,26 @@
+#include "AnimationSpritePlayground.h"
+
+AnimationSpritePlayground::AnimationSpritePlayground(std::string name)
+{
+}
+
+void AnimationSpritePlayground::InitScene(float windowWidth, float windowHeight)
+{
+}
+
+void AnimationSpritePlayground::Update()
+{
+	Scene::AdjustScrollOffset();
+}
+
+void AnimationSpritePlayground::KeyboardHold()
+{
+}
+
+void AnimationSpritePlayground::KeyboardDown()
+{
+}
+
+void AnimationSpritePlayground::KeyboardUp()
+{
+}
---
+++ Essential Math Assignment 1/Essential Math Tutorial 3/SpriteLib3.0-v2.0/Player.cpp
@@ -0,0 +1,29 @@
+#include "Player.h"
+
+Player::Player()
+{
+}
+
+Player::Player(std::string& fileName, std::string& animationJSON, int width, int height, Sprite* sprite, AnimationController* controller, Transform* transform, bool hasPhys, PhysicsBody* body)
+{
+}
+
+void Player::InitPlayer(std::string& fileName, std::string& animationJSON, int width, int height, Sprite* sprite, AnimationController* controller, Transform* transform, bool hasPhys, PhysicsBody* body)
+{
+}
+
+void Player::Update()
+{
+}
+
+void Player::MovementUpdate()
+{
+}
+
+void Player::AnimationUpdate()
+{
+}
+
+void Player::SetActiveAnimation(int anim)
+{
+}
