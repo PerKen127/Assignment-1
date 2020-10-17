ccb77ead5067d3dfd2f72720ee3929aba636fe1e
Josh <jkeny127@gmai.com>
2020-10-16 23:26:46 -0400

    The unfinished-finished project

--- Essential Math Assignment 1/Essential Math Tutorial 3/SpriteLib3.0-v2.0/Assignment1.cpp
+++ Essential Math Assignment 1/Essential Math Tutorial 3/SpriteLib3.0-v2.0/Assignment1.cpp
@@ -37,12 +37,12 @@ void Assignment1::InitScene(float windowWidth, float windowHeight)
 		ECS::AttachComponent<HorizontalScroll>(entity);
 		ECS::AttachComponent<VerticalScroll>(entity);
 
-		vec4 temp = vec4(-200.f, 200.f, -200.f, 200.f);
+		vec4 temp = vec4(-400.f, 400.f, -400.f, 400.f);
 		ECS::GetComponent<Camera>(entity).SetOrthoSize(temp);
 		ECS::GetComponent<Camera>(entity).SetWindowSize(vec2(float(windowWidth), float(windowHeight)));
 		ECS::GetComponent<Camera>(entity).Orthographic(aspectRatio, temp.x, temp.y, temp.z, temp.w, -100.f, 100.f);
 
-		//attaches the camera to vert and horiz scrolls
+		//attaches the camera to vertical and horizontal scrolls
 		ECS::GetComponent<HorizontalScroll>(entity).SetCam(&ECS::GetComponent<Camera>(entity));
 		ECS::GetComponent<VerticalScroll>(entity).SetCam(&ECS::GetComponent<Camera>(entity));
 	}
@@ -57,8 +57,8 @@ void Assignment1::InitScene(float windowWidth, float windowHeight)
 		ECS::AttachComponent<Transform>(entity);
 
 		//set up the components
-		std::string fileName = "BackgroundPlaceholder.png";
-		ECS::GetComponent<Sprite>(entity).LoadSprite(fileName, 2000, 2000);
+		std::string fileName = "platformerbackground.png";
+		ECS::GetComponent<Sprite>(entity).LoadSprite(fileName, 1500, 1500);
 		ECS::GetComponent<Transform>(entity).SetPosition(vec3(0.f, 0.f, 0.f));
 	}
 
@@ -86,7 +86,7 @@ void Assignment1::InitScene(float windowWidth, float windowHeight)
 		b2Body* tempBody;
 		b2BodyDef tempDef;
 		tempDef.type = b2_dynamicBody;
-		tempDef.position.Set(float32(0.f), float32(30.f));
+		tempDef.position.Set(float32(-65.f), float32(30.f));
 		tempDef.angle = Transform::ToRadians(0.f);
 
 		tempBody = m_physicsWorld->CreateBody(&tempDef);
@@ -94,37 +94,6 @@ void Assignment1::InitScene(float windowWidth, float windowHeight)
 		tempPhsBody = PhysicsBody(tempBody, float(tempSpr.GetWidth() - shrinkX), float(tempSpr.GetHeight() - shrinkY), vec2(0.f, 0.f), false);
 
 		ECS::GetComponent<PhysicsBody>(MainEntities::MainPlayer()).GetBody()->SetFixedRotation(true);
-	}
-
-	//setup ground platform
-	{
-		//Creates entity
-		auto entity = ECS::CreateEntity();
-
-		//Add components
-		ECS::AttachComponent<Sprite>(entity);
-		ECS::AttachComponent<Transform>(entity);
-		ECS::AttachComponent<PhysicsBody>(entity);
-
-		//Sets up components
-		std::string fileName = "GroundPlaceholder.jpg";
-		ECS::GetComponent<Sprite>(entity).LoadSprite(fileName, 2000, 20);
-		ECS::GetComponent<Transform>(entity).SetPosition(vec3(30.f, -20.f, 2.f));
-
-		auto& tempSpr = ECS::GetComponent<Sprite>(entity);
-		auto& tempPhsBody = ECS::GetComponent<PhysicsBody>(entity);
-
-		float shrinkX = 0.f;
-		float shrinkY = 0.f;
-		b2Body* tempBody;
-		b2BodyDef tempDef;
-		tempDef.type = b2_staticBody;
-		tempDef.position.Set(float32(30.f), float32(-20.f));
-
-		tempBody = m_physicsWorld->CreateBody(&tempDef);
-
-		tempPhsBody = PhysicsBody(tempBody, float(tempSpr.GetWidth() - shrinkX), float(tempSpr.GetHeight() - shrinkY), vec2(0.f, 0.f), false);
-
 	}
 
 	//setup platform 1
@@ -150,7 +119,7 @@ void Assignment1::InitScene(float windowWidth, float windowHeight)
 		b2Body* tempBody;
 		b2BodyDef tempDef;
 		tempDef.type = b2_staticBody;
-		tempDef.position.Set(float32(35.f), float32(20.f));
+		tempDef.position.Set(float32(-65.f), float32(20.f));
 
 		tempBody = m_physicsWorld->CreateBody(&tempDef);
 
@@ -181,7 +150,100 @@ void Assignment1::InitScene(float windowWidth, float windowHeight)
 		b2Body* tempBody;
 		b2BodyDef tempDef;
 		tempDef.type = b2_staticBody;
-		tempDef.position.Set(float32(180.f), float32(30.f));
+		tempDef.position.Set(float32(80.f), float32(30.f));
+
+		tempBody = m_physicsWorld->CreateBody(&tempDef);
+
+		tempPhsBody = PhysicsBody(tempBody, float(tempSpr.GetWidth() - shrinkX), float(tempSpr.GetHeight() - shrinkY), vec2(0.f, 0.f), false);
+
+	}
+
+	//setup platform 3
+	{
+		//Creates entity
+		auto entity = ECS::CreateEntity();
+
+		//Add components
+		ECS::AttachComponent<Sprite>(entity);
+		ECS::AttachComponent<Transform>(entity);
+		ECS::AttachComponent<PhysicsBody>(entity);
+
+		//Sets up components
+		std::string fileName = "GroundPlaceholder.jpg";
+		ECS::GetComponent<Sprite>(entity).LoadSprite(fileName, 75, 10);
+		ECS::GetComponent<Transform>(entity).SetPosition(vec3(30.f, -20.f, 2.f));
+
+		auto& tempSpr = ECS::GetComponent<Sprite>(entity);
+		auto& tempPhsBody = ECS::GetComponent<PhysicsBody>(entity);
+
+		float shrinkX = 0.f;
+		float shrinkY = 0.f;
+		b2Body* tempBody;
+		b2BodyDef tempDef;
+		tempDef.type = b2_staticBody;
+		tempDef.position.Set(float32(225.f), float32(50.f));
+
+		tempBody = m_physicsWorld->CreateBody(&tempDef);
+
+		tempPhsBody = PhysicsBody(tempBody, float(tempSpr.GetWidth() - shrinkX), float(tempSpr.GetHeight() - shrinkY), vec2(0.f, 0.f), false);
+
+	}
+
+	//setup platform 4
+	{
+		//Creates entity
+		auto entity = ECS::CreateEntity();
+
+		//Add components
+		ECS::AttachComponent<Sprite>(entity);
+		ECS::AttachComponent<Transform>(entity);
+		ECS::AttachComponent<PhysicsBody>(entity);
+
+		//Sets up components
+		std::string fileName = "GroundPlaceholder.jpg";
+		ECS::GetComponent<Sprite>(entity).LoadSprite(fileName, 75, 10);
+		ECS::GetComponent<Transform>(entity).SetPosition(vec3(30.f, -20.f, 2.f));
+
+		auto& tempSpr = ECS::GetComponent<Sprite>(entity);
+		auto& tempPhsBody = ECS::GetComponent<PhysicsBody>(entity);
+
+		float shrinkX = 0.f;
+		float shrinkY = 0.f;
+		b2Body* tempBody;
+		b2BodyDef tempDef;
+		tempDef.type = b2_staticBody;
+		tempDef.position.Set(float32(375.f), float32(40.f));
+
+		tempBody = m_physicsWorld->CreateBody(&tempDef);
+
+		tempPhsBody = PhysicsBody(tempBody, float(tempSpr.GetWidth() - shrinkX), float(tempSpr.GetHeight() - shrinkY), vec2(0.f, 0.f), false);
+
+	}
+
+	//setup platform 4
+	{
+		//Creates entity
+		auto entity = ECS::CreateEntity();
+
+		//Add components
+		ECS::AttachComponent<Sprite>(entity);
+		ECS::AttachComponent<Transform>(entity);
+		ECS::AttachComponent<PhysicsBody>(entity);
+
+		//Sets up components
+		std::string fileName = "GroundPlaceholder.jpg";
+		ECS::GetComponent<Sprite>(entity).LoadSprite(fileName, 75, 10);
+		ECS::GetComponent<Transform>(entity).SetPosition(vec3(30.f, -20.f, 2.f));
+
+		auto& tempSpr = ECS::GetComponent<Sprite>(entity);
+		auto& tempPhsBody = ECS::GetComponent<PhysicsBody>(entity);
+
+		float shrinkX = 0.f;
+		float shrinkY = 0.f;
+		b2Body* tempBody;
+		b2BodyDef tempDef;
+		tempDef.type = b2_staticBody;
+		tempDef.position.Set(float32(520.f), float32(20.f));
 
 		tempBody = m_physicsWorld->CreateBody(&tempDef);
 
@@ -203,8 +265,7 @@ void Assignment1::KeyboardHold()
 	auto& player = ECS::GetComponent<PhysicsBody>(MainEntities::MainPlayer());
 	float speed = 10.f;
 	b2Vec2 vel = b2Vec2(0.f, 0.f);
-	
-	
+		
 	if (Input::GetKey(Key::Shift))
 	{
 		speed *= 7.f;
@@ -213,8 +274,13 @@ void Assignment1::KeyboardHold()
 	if (Input::GetKey(Key::W) || Input::GetKey(Key::UpArrow) || Input::GetKey(Key::Space))
 	{
 		jumpTimer++;
-		
-		vel += b2Vec2(0.f, 2.f);
+			
+			vel += b2Vec2(0.f, 2.f);
+
+		if (vel == b2Vec2(0.f, 0.f) || vel == b2Vec2(1.f, 0.f))
+		{
+			vel += b2Vec2(0.f, -2.f);
+		}
 
 		if (jumpTimer >= 250)
 		{
@@ -223,24 +289,54 @@ void Assignment1::KeyboardHold()
 		
 	}
 
-	if (Input::GetKey(Key::A) || Input::GetKey(Key::LeftArrow))
+	if (toggle == 1)
 	{
-		for (int i = 0; i < 1000; i++)
+		if (Input::GetKey(Key::A) || Input::GetKey(Key::LeftArrow))
 		{
-			if (i == 0 || i % 50 == 0)
+			for (int i = 0; i < 1000; i++)
 			{
-				vel += b2Vec2(-0.05f, 0.f);
+				if (i == 0 || i % 100 == 0)
+				{
+					vel += b2Vec2(-0.1f, 0.f);
+				}
+			}
+		}
+
+		if (Input::GetKey(Key::D) || Input::GetKey(Key::RightArrow))
+		{
+			for (int i = 0; i < 1000; i++)
+			{
+				if (i == 0 || i % 100 == 0)
+				{
+					vel += b2Vec2(0.1f, 0.f);
+				}
 			}
 		}
 	}
 
-	if (Input::GetKey(Key::D) || Input::GetKey(Key::RightArrow))
+	if (toggle == 0)
 	{
-		for (int i = 0; i < 1000; i++)
+		if (Input::GetKey(Key::A) || Input::GetKey(Key::LeftArrow))
 		{
-			if (i == 0 || i % 50 == 0)
+			runTimer++;
+
+			vel += b2Vec2(-0.5f, 0.f);
+
+			if (runTimer == 1000)
 			{
-				vel += b2Vec2(0.05f, 0.f);
+				vel += b2Vec2(-0.5f, 0.f);
+			}
+		}
+
+		if (Input::GetKey(Key::D) || Input::GetKey(Key::RightArrow))
+		{
+			runTimer++;
+
+			vel += b2Vec2(0.5f, 0.f);
+
+			if (runTimer == 1000)
+			{
+				vel += b2Vec2(0.5f, 0.f);
 			}
 		}
 	}
@@ -279,6 +375,7 @@ void Assignment1::KeyboardUp()
 
 	if (Input::GetKeyUp(Key::A) || Input::GetKeyUp(Key::LeftArrow))
 	{
+		runTimer = 0;
 
 		for (int i = 0; i < 1000; i++)
 		{
@@ -291,6 +388,8 @@ void Assignment1::KeyboardUp()
 
 	if (Input::GetKeyUp(Key::D) || Input::GetKeyUp(Key::RightArrow))
 	{
+		runTimer = 0;
+
 		for (int i = 0; i < 1000; i++)
 		{
 			if (i == 0 || i % 100 == 0)
